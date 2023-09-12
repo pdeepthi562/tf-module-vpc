a012965964c1c8d4ae9496572ef1d4050ab15982
dpathim <dpdevops6@gmail.com>
2023-09-11 20:24:28 -0400

    Terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "main" {
   for_each = var.subnets
-  vpc_id     = aws_vpc_id
+  vpc_id     = var.vpc_id
   cidr_block = each.value["cidr"]
   availability_zone = each.value["az"]
 
