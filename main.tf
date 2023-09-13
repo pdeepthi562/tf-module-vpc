9bdd6fe50bc3190badb3f725d0caf3b1a0df42da
dpathim <dpdevops6@gmail.com>
2023-09-12 22:28:39 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -25,25 +25,25 @@ resource "aws_route" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  count = length(local.public_subnet_ids)
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
   domain = "vpc"
 }
 
 
 resource "aws_nat_gateway" "ngw" {
-  count = length(local.public_subnet_ids)
-  allocation_id = element(aws_eip.ngw.*.id, count.index)
-  subnet_id     = element(local.public_subnet_ids, count.index)
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  allocation_id = lookup(lookup(aws_eip.ngw, "each.key", null), "id", null)
+  subnet_id     = each.value["id"]
 }
 
-resource "aws_route" "ngw" {
-  count = length(local.private_route_table_ids)
-  route_table_id = element(local.private_route_table_ids, count.index)
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id     = element(aws_nat_gateway.ngw.*.id, count.index)
-}
+#resource "aws_route" "ngw" {
+#  count = length(local.private_route_table_ids)
+#  route_table_id = element(local.private_route_table_ids, count.index)
+#  destination_cidr_block    = "0.0.0.0/0"
+#  nat_gateway_id     = element(aws_nat_gateway.ngw.*.id, count.index)
+#}
 
 
-output "public_subnet_ids" {
-  value = local.public_subnet_ids
+output "subnet" {
+  value = module.subnets
 }
