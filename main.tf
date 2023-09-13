64e54d374bc63ee22f705631f0f965d91e69b41c
dpathim <dpdevops6@gmail.com>
2023-09-13 16:23:07 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -16,7 +16,34 @@ resource "aws_internet_gateway" "igw" {
     Name = "main"
   }
 }
+## created till natgateway
+#resource "aws_route" "igw" {
+#  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+#  route_table_id            = each.value["id"]
+#  destination_cidr_block    = "0.0.0.0/0"
+#  gateway_id = aws_internet_gateway.igw.id
+#}
 
+#resource "aws_eip" "ngw" {
+#  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+#  domain = "vpc"
+#}
+#
+#
+#resource "aws_nat_gateway" "ngw" {
+#  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+#  allocation_id = lookup(lookup(aws_eip.ngw, each.key, null), "id", null)
+#  subnet_id     = each.value["id"]
+#}
+#
+
+#
+#
+#output "subnet" {
+#  value = module.subnets
+#}
+
+ #Adding Nate gateway to other rout tables except public rout table
 resource "aws_route" "igw" {
   for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
   route_table_id            = each.value["id"]
@@ -25,25 +52,24 @@ resource "aws_route" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  count = length(local.public_subnet_ids)
   domain = "vpc"
 }
 
 
 resource "aws_nat_gateway" "ngw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
-  allocation_id = lookup(lookup(aws_eip.ngw, each.key, null), "id", null)
-  subnet_id     = each.value["id"]
+  count = length(local.public_subnet_ids)
+  allocation_id = element(aws_eip.ngw.*.id, count.index)
+  subnet_id     = element(local.public_subnet_ids, count.index)
 }
 
-#resource "aws_route" "ngw" {
-#  count = length(local.private_route_table_ids)
-#  route_table_id = element(local.private_route_table_ids, count.index)
-#  destination_cidr_block    = "0.0.0.0/0"
-#  nat_gateway_id     = element(aws_nat_gateway.ngw.*.id, count.index)
-#}
 
-
-output "subnet" {
-  value = module.subnets
+resource "aws_route" "ngw" {
+  count = length(local.private_route_table_ids)
+  route_table_id = element(local.private_route_table_ids, count.index)
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id     = element(aws_nat_gateway.ngw.*.id, count.index)
+}
+output "public_subnet_ids" {
+  value = local.public_subnet_ids
 }
