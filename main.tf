45c042c56dd0bbbedac107bc9df6902d5032a140
dpathim <dpdevops6@gmail.com>
2023-09-14 16:26:21 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "aws_vpc_peering_connection" "peering" {
 resource "aws_route" "peer" {
   count                     = length(local.private_route_table_ids)
   route_table_id            = element(local.private_route_table_ids, count.index)
-  destination_cidr_block    = "var.default_vpc_cidr"
+  destination_cidr_block    = var.default_vpc_cidr
   vpc_peering_connection_id  = aws_vpc_peering_connection.peering.id
 }
 
