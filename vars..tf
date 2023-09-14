121a0d0f52351ac53aab8b3fdc705e988aa5756b
dpathim <dpdevops6@gmail.com>
2023-09-14 16:09:41 -0400

    Terraform

--- vars..tf
+++ vars..tf
@@ -1,5 +1,7 @@
 variable "cidr" {}
-variable"subnets" {}
-variable "default_vpc_id" {
-  default = "vpc-0ea00bb90d84e71e1"
-}
+variable "subnets" {}
+variable "default_vpc_id" {default = "vpc-0ea00bb90d84e71e1"}
+variable "default_vpc_cidr" {}
+variable "default_vpc_route_table_id" {}
+
+
