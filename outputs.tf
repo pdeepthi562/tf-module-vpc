94cf004a95141db24e843ca4b5b0aec0f5e1ff2c
dpathim <dpdevops6@gmail.com>
2023-09-19 20:30:50 -0400

    Terraform

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,12 @@
-#output "subnet" {
-#  value = module.subnets
-#}
+output "subnet" {
+  value = module.subnets
+}
 
 
 output "public_subnet_ids" {
   value = local.public_subnet_ids
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
